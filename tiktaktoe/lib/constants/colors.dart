fbc78887e60589ad223a49f2f612f6fb314ab92c
Buraktrcn <112096531+Buraktrcn@users.noreply.github.com>
2024-05-07 13:50:29 +0300

    tiktaktoe game

---
+++ tiktaktoe/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 0, 0, 0);
+  static Color secondaryColor = Color.fromARGB(255, 255, 255, 255);
+  static Color accentColor = Color.fromARGB(255, 62, 217, 139);
+}
